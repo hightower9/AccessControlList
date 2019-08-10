21bc1f525031c04c3623f676b4fb439c886fe58f
alister <alister.messi@gmail.com>
2019-08-10 13:21:07 +0100

    Navbar Fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,12 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script>
+        window.Laravel = {!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!};
+    </script>
+    <script src="https://use.fontawesome.com/9712be8772.js"></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
@@ -20,99 +26,80 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/styles.css') }}" rel="stylesheet">
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-    <!-- Scripts -->
-    <script>
-        window.Laravel = {!! json_encode([
-            'csrfToken' => csrf_token(),
-        ]) !!};
-    </script>
-    <script src="https://use.fontawesome.com/9712be8772.js"></script>
 </head>
 <body>
-  <div id="app">
-      <nav class="navbar navbar-default navbar-static-top">
-          <div class="container">
-              <div class="navbar-header">
+    <div id="app">
+        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/') }}">
+                    {{ config('app.name', 'Laravel') }}
+                </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                  <!-- Collapsed Hamburger -->
-                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
-                      <span class="sr-only">Toggle Navigation</span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                      <span class="icon-bar"></span>
-                  </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        <li><a href="{{ url('/') }}">Home</a></li>
+                        @if (!Auth::guest())
+                            <li><a href="{{ route('posts.create') }}">New Article</a></li>
+                         @endif
+                    </ul>
 
-                  <!-- Branding Image -->
-                  <a class="navbar-brand" href="{{ url('/') }}">
-                      {{ config('app.name', 'Laravel') }}
-                  </a>
-              </div>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
 
-              <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                  <!-- Left Side Of Navbar -->
-                  <ul class="nav navbar-nav">
-                      <li><a href="{{ url('/') }}">Home</a></li>
-                      @if (!Auth::guest())
-                          <li><a href="{{ route('posts.create') }}">New Article</a></li>
-                       @endif
-                  </ul>
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        @role('Admin') {{-- Laravel-permission blade helper --}}
+                                        <a href="#"><i class="fa fa-btn fa-unlock"></i>Admin</a>
+                                        @endrole
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
 
-                  <!-- Right Side Of Navbar -->
-                  <ul class="nav navbar-nav navbar-right">
-                      <!-- Authentication Links -->
-                      @if (Auth::guest())
-                          <li><a href="{{ route('login') }}">Login</a></li>
-                          <li><a href="{{ route('register') }}">Register</a></li>
-                      @else
-                          <li class="dropdown">
-                              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                                  {{ Auth::user()->name }} <span class="caret"></span>
-                              </a>
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-                              <ul class="dropdown-menu" role="menu">
-                                  <li>
-                                      @role('Admin') {{-- Laravel-permission blade helper --}}
-                                      <a href="#"><i class="fa fa-btn fa-unlock"></i>Admin</a>
-                                      @endrole
-                                      <a href="{{ route('logout') }}"
-                                          onclick="event.preventDefault();
-                                                   document.getElementById('logout-form').submit();">
-                                          Logout
-                                      </a>
+         @if(Session::has('flash_message'))
+            <div class="container">      
+                <div class="alert alert-success"><em> {!! session('flash_message') !!}</em>
+                </div>
+            </div>
+        @endif 
 
-                                      <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                          {{ csrf_field() }}
-                                      </form>
-                                  </li>
-                              </ul>
-                          </li>
-                      @endif
-                  </ul>
-              </div>
-          </div>
-      </nav>
+        <!-- <div class="row">
+            <div class="col-md-8 col-md-offset-2">   -->            
+                @include ('errors.list') {{-- Including error file --}}
+          <!--   </div>
+        </div> -->
 
-      @if(Session::has('flash_message'))
-          <div class="container">
-              <div class="alert alert-success"><em> {!! session('flash_message') !!}</em>
-              </div>
-          </div>
-      @endif
-
-      <div class="row">
-          <div class="col-md-8 col-md-offset-2">
-              @include ('errors.list') {{-- Including error file --}}
-          </div>
-      </div>
-
-      @yield('content')
-
-  </div>
-
-  <!-- Scripts -->
-  <script src="{{ asset('js/app.js') }}"></script>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
